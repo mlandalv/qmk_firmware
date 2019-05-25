02c8707cf53513ca2220e4011b65692430b0289c
Martin Landälv <martin@landalv.se>
2019-05-25 20:51:22 +0200

    grave on single when caps pressed

--- keyboards/dz60/keymaps/60_martin/keymap.c
+++ keyboards/dz60/keymaps/60_martin/keymap.c
@@ -8,7 +8,6 @@ enum keyboard_layers {
   _CL      // Control Layer
 };
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BL] = LAYOUT_60_martin(
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
@@ -20,15 +19,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_FL] = LAYOUT_60_martin(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,            KC_DEL,
 		_______,          _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS,   RESET,
-		_______,          KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, KC_INS,  KC_HOME, KC_PGUP, _______,
+		_______,          KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, KC_INS,  KC_HOME,  KC_GRV, _______,
 		_______,          _______, _______, _______, _______, _______, _______, _______, _______, KC_END,  KC_PGDN,            KC_UP, _______,
 		_______, _______,          _______,                   _______,                                     _______, KC_LEFT, KC_DOWN, KC_RGHT
 	),
 	[_CL] = LAYOUT_60_martin(
-		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+         KC_GRV, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           KC_DEL,
 		_______,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______,   RESET,
 		_______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-		_______,          _______, _______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______,          _______, _______,
-		_______, _______,          _______,                   _______,                                     _______, _______, _______, _______
+		_______,          _______, _______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______,          KC_PGUP, _______,
+		_______, _______,          _______,                   _______,                                     _______, KC_HOME, KC_PGDN, KC_END
 	)
 };

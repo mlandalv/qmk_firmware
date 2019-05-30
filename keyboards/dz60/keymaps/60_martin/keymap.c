399f3b048155a6d9bbcc63010b42d7cda1298165
Martin Landälv <martin@landalv.se>
2019-05-30 20:27:47 +0200

    dz60: layers and leader

--- keyboards/dz60/keymaps/60_martin/keymap.c
+++ keyboards/dz60/keymaps/60_martin/keymap.c
@@ -3,29 +3,65 @@
 enum keyboard_layers {
   _BL = 0, // Base Layer
   _FL,     // Function Layer
-  _CL      // Control Layer
+  _CL,     // Control Layer
+  _WS      // WebStorm
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BL] = LAYOUT_60_martin(
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
 		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        MO(_FL),          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,
+        MO(_FL),          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          MO(_CL),
-		KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                                      KC_RALT, KC_RGUI, KC_MENU,  KC_RCTL
+		KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                                      KC_RALT, KC_RGUI, KC_MENU, KC_RCTL
 	),
 	[_FL] = LAYOUT_60_martin(
-		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,            KC_DEL,
-		_______,          _______, KC_UP,   _______, _______, _______, _______, _______,  KC_INS, _______, KC_PSCR, KC_SLCK, KC_PAUS,   RESET,
-		_______,          KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_HOME,  KC_GRV, _______,
-		_______,          _______, _______, _______, _______, _______, _______, _______, _______, KC_END,  KC_PGDN,            KC_UP, _______,
-		_______, _______,          _______,                   _______,                                     _______, KC_LEFT, KC_DOWN, KC_RGHT
+		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
+		_______,          _______, KC_UP,   _______, _______, _______, _______, _______, KC_INS,  _______, KC_PSCR, KC_SLCK, KC_PAUS, RESET,
+		_______,          KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_HOME, KC_GRV,  _______,
+		_______,          _______, _______, _______, _______, _______, _______, _______, KC_LEAD, KC_END,  KC_PGDN,          KC_UP,   _______,
+		_______, _______, _______,                            _______,                                     _______, KC_LEFT, KC_DOWN, KC_RGHT
 	),
 	[_CL] = LAYOUT_60_martin(
-         KC_GRV, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           KC_DEL,
-		_______,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______,   RESET,
+        KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_DEL,
+		_______,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, RESET,
         KC_CAPS,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-		_______,          _______, _______, _______, _______,  BL_DEC, BL_TOGG, BL_INC,  BL_STEP, _______, _______,          KC_PGUP, _______,
-		_______, _______,          _______,                   _______,                                     _______, KC_HOME, KC_PGDN, KC_END
-	)
+		_______,          _______, _______, _______, _______, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, _______, _______,          KC_PGUP, _______,
+		_______, _______, _______,                            _______,                                     _______, KC_HOME, KC_PGDN, KC_END
+	),
+    [_WS] = LAYOUT_60_martin(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+        _______, _______, _______,                            _______,                                     _______, _______, _______, _______
+    )
 };
+
+LEADER_EXTERNS();
+
+void matrix_scan_user(void) {
+    LEADER_DICTIONARY() {
+        leading = false;
+        leader_end();
+
+        SEQ_TWO_KEYS(KC_G, KC_A) {
+            SEND_STRING("git add -A");
+        }
+        SEQ_TWO_KEYS(KC_G, KC_P) {
+            SEND_STRING("git push");
+        }
+        SEQ_THREE_KEYS(KC_G, KC_P, KC_T) {
+            SEND_STRING("git push --tags");
+        }
+        SEQ_TWO_KEYS(KC_G, KC_C) {
+            SEND_STRING("git commit -m \"\""SS_TAP(X_LEFT));
+        }
+//        SEQ_TWO_KEYS(KC_A, KC_S) {
+//            register_code(KC_LGUI);
+//            register_code(KC_S);
+//            unregister_code(KC_S);
+//            unregister_code(KC_LGUI);
+//        }
+    }
+}

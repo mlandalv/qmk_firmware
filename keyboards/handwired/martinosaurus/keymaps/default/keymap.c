4392cb6dfc28a196ac22efe05c78cbcdb66b88c2
Martin Landälv <martin@landalv.se>
2019-08-01 17:00:58 +0200

    layout

--- keyboards/handwired/martinosaurus/keymaps/default/keymap.c
+++ keyboards/handwired/martinosaurus/keymaps/default/keymap.c
@@ -1,42 +1,43 @@
 #include QMK_KEYBOARD_H
 
 enum keyboard_layers {
-    _BL = 0, // Base Layer
-    _FL,     // Function Layer
-    _NP,     // Numpad
+    _BASE = 0, // Base Layer
+    _RAISE,    // Raise
+    _LOWER,    // Lower
     _WS      // WebStorm
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_BL] = LAYOUT_martinosaurus(
-        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, // KC_EQL,           KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, // KC_RBRC, KC_BSLS,
-        MO(_FL), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, //         KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, //         MO(_CL),
-        KC_LCTL, KC_LGUI, KC_LALT, MO(_NP),     KC_SPC,                      KC_BSPC, KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+[_BASE] = LAYOUT_martinosaurus(
+        KC_GESC,    KC_1,    KC_2,    KC_3,       KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, // KC_EQL,           KC_BSPC,
+        KC_TAB,     KC_Q,    KC_W,    KC_E,       KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, // KC_RBRC, KC_BSLS,
+        MO(_RAISE), KC_A,    KC_S,    KC_D,       KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, //         KC_ENT,
+        KC_LSFT,    KC_Z,    KC_X,    KC_C,       KC_V,    KC_B,                   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, //         MO(_CL),
+        KC_LCTL,    KC_LGUI, KC_LALT, MO(_LOWER),     KC_SPC,                      KC_BSPC, KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
-[_FL] = LAYOUT_martinosaurus(
+[_RAISE] = LAYOUT_martinosaurus(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  // KC_F12,           KC_DEL,
-        _______, _______, _______, _______, _______, _______,                _______, _______, KC_INS,  KC_LBRC, KC_RBRC, KC_EQL,  // KC_PSCR, KC_RBRC,KC_SLCK, //KC_PAUS, RESET,
+        _______, KC_F12,  _______, _______, _______, _______,                _______, _______, KC_INS,  KC_LBRC, KC_RBRC, KC_EQL,  // KC_PSCR, KC_RBRC,KC_SLCK, //KC_PAUS, RESET,
         _______, _______, _______, _______, _______, _______,                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_GRV,  // _______,
         _______, _______, _______, _______, _______, _______,                _______, _______, KC_LEAD, _______, _______, _______, // _______,
         _______, _______, _______, _______,    _______,                       KC_DEL, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
-[_NP] = LAYOUT_martinosaurus(
-        _______, _______, _______, _______, _______, _______,                _______, _______, KC_NUMLOCK, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS,
-        _______, _______, _______, _______, _______, _______,                _______, _______, KC_KP_7,    KC_KP_8,     KC_KP_9,        KC_KP_PLUS,
-        KC_CAPS, _______, _______, _______, _______, _______,                _______, _______, KC_KP_4,    KC_KP_5,     KC_KP_6,        KC_KP_PLUS,
-        _______, _______, _______, _______, _______, _______,                _______, _______, KC_KP_1,    KC_KP_2,     KC_KP_3,        KC_KP_ENTER,
-        _______, _______, _______, _______,    _______,                      _______, _______, KC_KP_0,    KC_KP_0,     KC_KP_DOT,      KC_KP_ENTER
+[_LOWER] = LAYOUT_martinosaurus(
+        _______, _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______, KC_EQL,
+        _______, _______, _______, _______, _______, _______,                KC_LPRN, KC_MINS, KC_EQL,  KC_RPRN, _______, KC_BSLS,
+        KC_CAPS, _______, _______, _______, _______, _______,                KC_LCBR, _______, _______, KC_RCBR, _______, _______,
+        _______, _______, _______, _______, _______, _______,                KC_LBRC, _______, _______, KC_RBRC, _______, _______,
+        _______, _______, _______, _______,    _______,                      _______, _______, _______, _______, _______, _______
 )};
-//[_WS] = LAYOUT_60_martin(
-//        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-//        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-//        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-//        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
-//        _______, _______, _______,                            _______,                                     _______, _______, _______, _______
-//)
-//};
+
+//[_LOWER] = LAYOUT_martinosaurus(
+//        _______, _______, _______, _______, _______, _______,                _______, _______, KC_NUMLOCK, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS,
+//        _______, _______, _______, _______, _______, _______,                _______, _______, KC_KP_7,    KC_KP_8,     KC_KP_9,        KC_KP_PLUS,
+//        KC_CAPS, _______, _______, _______, _______, _______,                _______, _______, KC_KP_4,    KC_KP_5,     KC_KP_6,        KC_KP_PLUS,
+//        _______, _______, _______, _______, _______, _______,                _______, _______, KC_KP_1,    KC_KP_2,     KC_KP_3,        KC_KP_ENTER,
+//        _______, _______, _______, _______,    _______,                      _______, _______, KC_KP_0,    KC_KP_0,     KC_KP_DOT,      KC_KP_ENTER
+//)};
+
 
 LEADER_EXTERNS();
 

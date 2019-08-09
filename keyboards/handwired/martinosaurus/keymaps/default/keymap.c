edfbf43a674a762eb29a716ca98812f3d520f439
Martin Landälv <martin@landalv.se>
2019-08-09 11:07:52 +0200

    move backspace

--- keyboards/handwired/martinosaurus/keymaps/default/keymap.c
+++ keyboards/handwired/martinosaurus/keymaps/default/keymap.c
@@ -11,22 +11,22 @@ enum keyboard_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_martinosaurus(
         KC_GESC,    KC_1,    KC_2,    KC_3,       KC_4,    KC_5,                   KC_6,    KC_7,        KC_8,    KC_9,    KC_0,    KC_MINS, // KC_EQL,           KC_BSPC,
-        KC_TAB,     KC_Q,    KC_W,    KC_E,       KC_R,    KC_T,                   KC_Y,    KC_U,        KC_I,    KC_O,    KC_P,    KC_BSPC, //KC_BSLS, // KC_RBRC, KC_BSLS,
-        KC_BSLS,    KC_A,    KC_S,    KC_D,       KC_F,    KC_G,                   KC_H,    KC_J,        KC_K,    KC_L,    KC_SCLN, KC_QUOT, //         KC_ENT,
+        KC_TAB,     KC_Q,    KC_W,    KC_E,       KC_R,    KC_T,                   KC_Y,    KC_U,        KC_I,    KC_O,    KC_P,    KC_BSLS, //KC_BSLS, // KC_RBRC, KC_BSLS,
+        KC_BSPC,    KC_A,    KC_S,    KC_D,       KC_F,    KC_G,                   KC_H,    KC_J,        KC_K,    KC_L,    KC_SCLN, KC_QUOT, //         KC_ENT,
         KC_LSFT,    KC_Z,    KC_X,    KC_C,       KC_V,    KC_B,                   KC_N,    KC_M,        KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, //         MO(_CL),
         KC_LCTL,    KC_LGUI, KC_LALT, MO(_LOWER),     KC_SPC,                      KC_ENT,  MO(_RAISE),  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 [_RAISE] = LAYOUT_martinosaurus(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  // KC_F12,           KC_DEL,
-        _______, KC_F12,  KC_LPRN, KC_RPRN, _______, _______,                _______, _______, KC_INS,  KC_LBRC, KC_RBRC, KC_DEL, // KC_EQL,  // KC_PSCR, KC_RBRC,KC_SLCK, //KC_PAUS, RESET,
-        KC_CAPS, _______, KC_LCBR, KC_RCBR, _______, _______,                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_GRV,  // _______,
+        _______, KC_F12,  KC_LPRN, KC_RPRN, _______, _______,                _______, _______, KC_INS,  KC_LBRC, KC_RBRC, _______, // KC_EQL,  // KC_PSCR, KC_RBRC,KC_SLCK, //KC_PAUS, RESET,
+        KC_DEL, _______, KC_LCBR, KC_RCBR, _______, _______,                 KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_GRV,  // _______,
         _______, _______, KC_LBRC, KC_RBRC, _______, _______,                _______, _______, KC_LEAD, _______, _______, _______, // _______,
         _______, _______, _______, _______,    _______,                       KC_DEL, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 [_LOWER] = LAYOUT_martinosaurus(
         _______, _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______, KC_EQL,
         _______, _______, _______, _______, _______, _______,                KC_EQL,  KC_MINS, KC_LPRN, KC_RPRN, _______, KC_DEL,
-        _______, _______, _______, _______, _______, TG(_CS),                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_EQL,
+        KC_CAPS, _______, _______, _______, _______, TG(_CS),                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_EQL,
         _______, _______, _______, _______, _______, _______,                KC_LBRC, _______, _______, KC_GT,   _______, _______,
         _______, _______, _______, _______,     _______,                     _______, _______, _______, _______, _______, _______
 ),

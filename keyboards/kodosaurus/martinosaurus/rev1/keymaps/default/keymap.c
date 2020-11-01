30cf20f10a378b16ab1f1d3fdc97308f124c5acc
Martin Landälv <martin@landalv.se>
2020-11-01 17:58:14 +0100

    feat: lite gitmacros

--- keyboards/kodosaurus/martinosaurus/rev1/keymaps/default/keymap.c
+++ keyboards/kodosaurus/martinosaurus/rev1/keymaps/default/keymap.c
@@ -17,8 +17,9 @@
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
-  QMKBEST = SAFE_RANGE,
-  QMKURL
+    GIT_A = SAFE_RANGE,
+    GIT_PT,
+    GIT_CM,
 };
 
 enum keyboard_layers {
@@ -27,6 +28,7 @@ enum keyboard_layers {
     _RAISE,
     _LOWER,
     _DN,
+    _MACRO,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -38,31 +40,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_LCTL,    KC_LGUI, MO(_FN), KC_LALT, MO(_LOWER), KC_SPC,             KC_ENT, MO(_RAISE),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
     [_FN] = LAYOUT_125(
-            KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                  KC_F6,   KC_F7,   KC_NUMLOCK, KC_KP_SLASH,  KC_KP_ASTERISK, KC_KP_MINUS,
-            _______, KC_F12,  _______, _______, _______, _______,                _______, _______, KC_KP_7,    KC_KP_8,      KC_KP_9,        KC_KP_PLUS,
-            KC_DEL,  _______, _______, _______, _______, _______,                _______, _______, KC_KP_4,    KC_KP_5,      KC_KP_6,        KC_KP_PLUS,
-            _______, _______, _______, _______, _______, _______,                _______, _______, KC_KP_1,    KC_KP_2,      KC_KP_3,        KC_KP_ENTER,
-            _______, _______, _______, _______, _______, _______,                _______, _______, KC_KP_0,    _______,      _______,        KC_KP_ENTER
+            KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,      KC_F5,               KC_F6,   KC_F7,   KC_NUMLOCK, KC_KP_SLASH,  KC_KP_ASTERISK, KC_KP_MINUS,
+            _______, KC_F12,  _______, _______, _______,    _______,             _______, _______, KC_KP_7,    KC_KP_8,      KC_KP_9,        KC_KP_PLUS,
+            KC_DEL,  _______, _______, _______, _______,    _______,             _______, _______, KC_KP_4,    KC_KP_5,      KC_KP_6,        KC_KP_PLUS,
+            _______, _______, _______, _______, _______,    _______,             _______, _______, KC_KP_1,    KC_KP_2,      KC_KP_3,        KC_KP_ENTER,
+            _______, _______, _______, _______, MO(_MACRO), _______,             _______, _______, KC_KP_0,    _______,      _______,        KC_KP_ENTER
     ),
     [_RAISE] = LAYOUT_125(
             KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
             _______, KC_F12,  KC_LPRN, KC_RPRN, _______, _______,                _______, _______, KC_INS,  KC_LBRC, KC_RBRC, _______,
-            KC_DEL,  _______, KC_LCBR, KC_RCBR, _______, _______,                KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, KC_GRV,
+            KC_DEL,  _______, KC_LCBR, KC_RCBR, _______, _______,                KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______, KC_GRV ,
             _______, _______, KC_LBRC, KC_RBRC, _______, _______,                _______, _______, _______, _______, _______, _______,
             KC_RCTL, _______, _______, _______, MO(_DN), _______,                _______, _______, _______, _______, _______, _______
     ),
     [_LOWER] = LAYOUT_125(
-            KC_GRV,  _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______, KC_EQL,
-            _______, _______, _______, _______, _______, _______,                _______, _______, _______, _______, KC_LBRC, KC_RBRC,
-            KC_DEL,  _______, _______, _______, _______, _______,                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_MINS, KC_EQL,
-            _______, _______, _______, _______, _______, _______,                KC_LBRC, _______, KC_LEAD, KC_GT,   _______, _______,
-            KC_RCTL, _______, _______, _______, _______, _______,                _______, MO(_DN), KC_HOME, KC_PGDN, KC_PGUP, KC_END
+            KC_GRV,  _______, _______,    _______, _______, _______,             _______, _______, _______, _______, _______, KC_EQL,
+            _______, _______, _______,    _______, _______, _______,             _______, _______, _______, _______, KC_LBRC, KC_RBRC,
+            KC_DEL,  _______, _______,    _______, _______, _______,             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_MINS, KC_EQL,
+            _______, _______, _______,    _______, _______, _______,             KC_LBRC, _______, KC_LEAD, KC_GT,   _______, _______,
+            KC_RCTL, _______, MO(_MACRO), _______, _______, _______,             _______, MO(_DN), KC_HOME, KC_PGDN, KC_PGUP, KC_END
     ),
     [_DN] = LAYOUT_125(
             RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
             _______, KC_F12,  KC_LPRN, KC_RPRN, _______, _______,                _______, _______, _______, _______, _______, _______,
-            KC_DEL,  _______, KC_LCBR, KC_RCBR, _______, _______,                _______, _______, _______, _______, _______, _______,
+            KC_DEL,  _______, KC_LCBR, KC_RCBR, _______, _______,                KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______, _______,
             _______, _______, KC_LBRC, KC_RBRC, _______, _______,                _______, _______, _______, _______, _______, _______,
+            _______, _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______, _______
+    ),
+    [_MACRO] = LAYOUT_125(
+            _______, _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______, _______,
+            _______, _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______, _______,
+            _______, _______, _______, _______, _______, _______,                GIT_A, GIT_PT, GIT_CM, _______, _______, _______,
+            _______, _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______, _______,
             _______, _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______, _______
     ),
 };
@@ -71,20 +80,28 @@ LEADER_EXTERNS();
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case QMKBEST:
+    case GIT_A:
       if (record->event.pressed) {
-        // when keycode QMKBEST is pressed
-        SEND_STRING("QMK is the best thing ever!");
+        // when keycode GIT_A is pressed
+        SEND_STRING("git add -A");
       } else {
-        // when keycode QMKBEST is released
+        // when keycode GIT_A is released
       }
       break;
-    case QMKURL:
+    case GIT_PT:
       if (record->event.pressed) {
-        // when keycode QMKURL is pressed
-        SEND_STRING("https://qmk.fm/" SS_TAP(X_ENTER));
+        // when keycode GIT_PT is pressed
+        SEND_STRING("git push; git push --tags");
       } else {
-        // when keycode QMKURL is released
+        // when keycode GIT_PT is released
+      }
+      break;
+    case GIT_CM:
+      if (record->event.pressed) {
+        // when keycode GIT_CM is pressed
+          SEND_STRING("git commit -m \"\"" SS_TAP(X_LEFT));
+      } else {
+        // when keycode GIT_PT is released
       }
       break;
   }
@@ -115,6 +132,15 @@ void matrix_scan_user(void) {
         SEQ_TWO_KEYS(KC_Y, KC_U) {
             SEND_STRING("yarn upgrade-interactive --latest");
         }
+        SEQ_TWO_KEYS(KC_Y, KC_L) {
+            SEND_STRING("yarn lint");
+        }
+//        SEQ_THREE_KEYS(KC_Y, KC_L, KC_L) {
+//            SEND_STRING("yarn lint && yarn typecheck");
+//        }
+//        SEQ_FOUR_KEYS(KC_Y, KC_L, KC_L, KC_L) {
+//            SEND_STRING("yarn lint && yarn typecheck && yarn test");
+//        }
 //        SEQ_TWO_KEYS(KC_A, KC_S) {
 //            register_code(KC_LGUI);
 //            register_code(KC_S);

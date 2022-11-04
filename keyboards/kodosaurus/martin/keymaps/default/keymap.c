aa5b3a204ee59103b9bf1a334a1c039c8092145b
Martin Landälv <martin@landalv.se>
2022-11-04 09:20:48 +0100

    move RALT to right side

--- keyboards/kodosaurus/martin/keymaps/default/keymap.c
+++ keyboards/kodosaurus/martin/keymaps/default/keymap.c
@@ -35,25 +35,25 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_CMAK] = LAYOUT_125( /* Colemak */
-             KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,       KC_5,                KC_6,    KC_7,      KC_8,    KC_9,    KC_0,    KC_MINS,
-             KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,       KC_B,                KC_J,    KC_L,      KC_U,    KC_Y, KC_SCLN,    KC_BSLS,
-             KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,       KC_G,                KC_M,    KC_N,      KC_E,    KC_I,    KC_O,    KC_QUOT,
-             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,       KC_V,                KC_K,    KC_H,      KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-             KC_LCTL, KC_LGUI, KC_RALT, KC_LALT, MO(_LOWER), KC_SPC,              KC_ENT, MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+            KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,       KC_5,                KC_6,    KC_7,      KC_8,    KC_9,    KC_0,    KC_MINS,
+            KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,       KC_B,                KC_J,    KC_L,      KC_U,    KC_Y, KC_SCLN,    KC_BSLS,
+            KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,       KC_G,                KC_M,    KC_N,      KC_E,    KC_I,    KC_O,    KC_QUOT,
+            KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,       KC_V,                KC_K,    KC_H,      KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+            KC_LCTL, KC_LGUI, MO(_FN), KC_LALT, MO(_LOWER), KC_SPC,              KC_ENT, MO(_RAISE), KC_RALT, KC_DOWN, KC_UP,   KC_RGHT
     ),
     [_QWER] = LAYOUT_125( /* QWERTY */
             KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,       KC_5,                KC_6,    KC_7,      KC_8,    KC_9,    KC_0,    KC_MINS,
             KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,                KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,    KC_BSLS,
             KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,       KC_G,                KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN, KC_QUOT,
             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,                KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-            KC_LCTL, KC_LGUI, KC_RALT, KC_LALT, MO(_LOWER), KC_SPC,              KC_ENT, MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+            KC_LCTL, KC_LGUI, MO(_FN), KC_LALT, MO(_LOWER), KC_SPC,              KC_ENT, MO(_RAISE), KC_RALT, KC_DOWN, KC_UP,   KC_RGHT
     ),
     [_RAISE] = LAYOUT_125(
-            KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,                  KC_F6,   KC_F7,     KC_F8,   KC_F9,   KC_F10,  KC_F11,
-            _______, KC_F12,  KC_LPRN, KC_RPRN, _______,   _______,                _______, _______,   KC_INS,  KC_LBRC, KC_RBRC, _______,
-            KC_DEL,  _______, KC_LCBR, KC_RCBR, _______,   _______,                KC_HOME, KC_PGDN,   KC_PGUP, KC_END , _______, KC_GRV ,
-            _______, _______, KC_LBRC, KC_RBRC, _______,   _______,                _______, _______,   _______, _______, _______, _______,
-            KC_RCTL, _______, _______, KC_RALT, MO(_XTRA), _______,                _______, _______,   _______, _______, _______, _______
+            KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,     KC_F5,                KC_F6,   KC_F7,     KC_F8,   KC_F9,   KC_F10,  KC_F11,
+            _______, KC_F12,  KC_LPRN, KC_RPRN, _______,   _______,              _______, _______,   KC_INS,  KC_LBRC, KC_RBRC, _______,
+            KC_DEL,  _______, KC_LCBR, KC_RCBR, _______,   _______,              KC_HOME, KC_PGDN,   KC_PGUP, KC_END , _______, KC_GRV ,
+            _______, _______, KC_LBRC, KC_RBRC, _______,   _______,              _______, _______,   _______, _______, _______, _______,
+            KC_RCTL, _______, _______, KC_RALT, MO(_XTRA), _______,              _______, _______,   _______, _______, _______, _______
     ),
     [_LOWER] = LAYOUT_125(
             KC_GRV,  KC_6,    KC_7,       KC_8,    KC_9,    KC_0,                _______, _______,   _______, _______, _______, KC_EQL,
